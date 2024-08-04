599a77c21626a0db0aa2cf1ec584aaf7b5672eb7
Monti Sangwan <montisangwan0403@gmail.com>
2024-08-04 11:21:54 +0530

    database fixed username

--- src/main/java/com/scm/service/UserServiceImpl.java
+++ src/main/java/com/scm/service/UserServiceImpl.java
@@ -2,11 +2,50 @@ package com.scm.service;
 
 import java.util.*;
 
+import com.scm.entities.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    
-    
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> getUserById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> updateUser(User user) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteUser(String id) {
+
+    }
+
+    @Override
+    public boolean isUserExist(String userId) {
+        return false;
+    }
+
+    @Override
+    public boolean isUserExistByEmail(String email) {
+        return false;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return null;
+    }
 }
